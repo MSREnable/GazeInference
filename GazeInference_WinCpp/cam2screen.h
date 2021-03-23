7a151b6a7f427b52011ebe7a0c359c0060495f95
Jatin Sharma <jatin.joy.sharma@gmail.com>
2021-03-23 11:37:14 -0700

    Adding cam2screen

---
+++ GazeInference_WinCpp/cam2screen.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "framework.h"
+#include <fstream>
+
+//std::ifstream devices("device_metrics_sku.json", std::ifstream::binary);
+
+std::map<std::string, float> deviceMetrics = {  {"xCameraToScreenDisplacementInCm", 13.0},
+                                                {"yCameraToScreenDisplacementInCm", 0.6},
+                                                {"widthScreenInCm", 26.0},
+                                                {"heightScreenInCm", 17.35},
+                                                {"ppi", 267}
+                                            };
+
+cv::Point cam2screen(float xDisplacementFromCameraInCm,
+                float yDisplacementFromCameraInCm,
+                int widthScreenInPoints,
+                int heightScreenInPoints)
+{
+
+    float xCameraToScreenDisplacementInCm = deviceMetrics["xCameraToScreenDisplacementInCm"];
+    float yCameraToScreenDisplacementInCm = deviceMetrics["yCameraToScreenDisplacementInCm"];
+    float widthScreenInCm = deviceMetrics["widthScreenInCm"];
+    float heightScreenInCm = deviceMetrics["heightScreenInCm"];
+
+    if (!xCameraToScreenDisplacementInCm || !yCameraToScreenDisplacementInCm ||
+        !widthScreenInCm || !heightScreenInCm) {
+        return cv::Point(0,0);
+    }
+
+    float xScreenInCm;
+    float yScreenInCm;
+    float xScreenInPoints;
+    float yScreenInPoints;
+    // Camera Offset and Screen Orientation compensation
+    // Camera above screen
+    // - Portrait on iOS devices
+    // - Landscape on Surface devices
+    xScreenInCm = xDisplacementFromCameraInCm + xCameraToScreenDisplacementInCm;
+    yScreenInCm = -yDisplacementFromCameraInCm - yCameraToScreenDisplacementInCm;
+    xScreenInPoints = xScreenInCm / widthScreenInCm * widthScreenInPoints;
+    yScreenInPoints = yScreenInCm / heightScreenInCm * heightScreenInPoints;
+
+    return cv::Point(xScreenInPoints, yScreenInPoints);
+}
+
+cv::Point screen2cam(float xScreenInPoints,
+    float yScreenInPoints,
+    float orientation,
+    float widthScreenInPoints,
+    float heightScreenInPoints)
+{
+
+    float xCameraToScreenDisplacementInCm = deviceMetrics["xCameraToScreenDisplacementInCm"];
+    float yCameraToScreenDisplacementInCm = deviceMetrics["yCameraToScreenDisplacementInCm"];
+    float widthScreenInCm = deviceMetrics["widthScreenInCm"];
+    float heightScreenInCm = deviceMetrics["heightScreenInCm"];
+
+    if (!xCameraToScreenDisplacementInCm ||
+        !yCameraToScreenDisplacementInCm ||
+        !widthScreenInCm ||
+        !heightScreenInCm) {
+        return cv::Point(0, 0);
+    }
+
+    float xScreenInCm;
+    float yScreenInCm;
+    float xDisplacementFromCameraInCm;
+    float yDisplacementFromCameraInCm;
+
+    // Camera Offset and Screen Orientation compensation
+    // Camera above screen
+    // - Portrait on iOS devices
+    // - Landscape on Surface devices
+    xScreenInCm = xScreenInPoints * widthScreenInCm / widthScreenInPoints;
+    yScreenInCm = yScreenInPoints * heightScreenInCm / heightScreenInPoints;
+    xDisplacementFromCameraInCm = xScreenInCm - xCameraToScreenDisplacementInCm;
+    yDisplacementFromCameraInCm = -yScreenInCm - yCameraToScreenDisplacementInCm;
+    
+    return cv::Point(xDisplacementFromCameraInCm, yDisplacementFromCameraInCm);
+}
+
+
